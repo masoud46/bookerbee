f1496abc15de2f1b3527ca66ef3198dd96d79ee4
Masoud <masoud46@yahoo.com>
2023-04-25 18:10:18 +0200

    Font Awesome test 2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,6 @@
-// import '@fortawesome/fontawesome-free/js/fontawesome.min'
-// import '@fortawesome/fontawesome-free/js/regular.min'
-// import '@fortawesome/fontawesome-free/js/solid.min'
+import '@fortawesome/fontawesome-free/js/fontawesome.min'
+import '@fortawesome/fontawesome-free/js/regular.min'
+import '@fortawesome/fontawesome-free/js/solid.min'
 
 
 /**
