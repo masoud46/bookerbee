27e20e06f6b29dbd457410cb21be9e8306d7da90
Masoud <masoud46@yahoo.com>
2023-06-24 18:49:32 +0200

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule): void {
 		$schedule->command("send:reminders")->everyFiveMinutes();
-		$schedule->command("app:monitoring")->everyTenMinutes();
+		$schedule->command("app:monitoring")->everyFiveMinutes();
 	}
 
 	/**
